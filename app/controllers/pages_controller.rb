ce9b99c027fce1d6f3eb5aede51eb4b61173b597
Isabelle Rousseau <rousseau_isabel@hotmail.com>
2020-03-30 14:11:00 +0200

    adjusted pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,4 +10,18 @@ class PagesController < ApplicationController
 
   def corona
   end
+
+   def greet
+    if Time.now.between? Time.now.beginning_of_day, Time.now.middle_of_day
+      return "Good Morning"
+    elsif Time.now.between? Time.now.middle_of_day, Time.now.change(hour: 17)
+      return "Good Afternoon"
+    elsif Time.now.between? Time.now.change(hour: 17), Time.now.change(hour: 20)
+      return "Good Evening"
+    elsif Time.now.between? Time.now.change(hour: 20), Time.now.end_of_day
+      return "Good Night"
+    end
+  end
+
+
 end
